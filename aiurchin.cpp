34a4ceca134991d64e8491a410fec5a356803c97
Amber Rowden <happysquirrel9@yahoo.com>
2016-04-05 14:52:10 -0400

    Update aiurchin.cpp

--- aiurchin.cpp
+++ aiurchin.cpp
@@ -12,9 +12,7 @@ AIUrchin::AIUrchin()
   */create urchin here*/
   radius = 40;
   setPixmap(QPixmap(":/images/resources/urchin.png").scaled(radius*2,radius*2)); // need to upload an image for urchin orb
-  do { // this is supposed to prevent aiorbs from being placed on top of each other but it doesn't seem to work
-   setPos(qrand() % 750, qrand() % 550);
-  } while (collidingItems().size() > 0);
+  setPos(qrand() % 750, qrand() % 550);
 }
 
 

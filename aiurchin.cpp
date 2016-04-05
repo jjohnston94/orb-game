9b27f8dcbd71b56fef79f829050266025ac4ce10
Amber Rowden <happysquirrel9@yahoo.com>
2016-04-05 14:23:12 -0400

    Update aiurchin.cpp

--- aiurchin.cpp
+++ aiurchin.cpp
@@ -15,6 +15,12 @@ AIUrchin::AIUrchin()
 AIUrchin::AIUrchin(qreal radius)
 {
   */create urchin here*/
+  this->radius = radius -.5; // .5 is being added to radius elsewhere in the program and I dont know why
+    setPixmap(QPixmap(":/images/resources/black.png").scaled(radius*2,radius*2)); // set the pixmap image and then scale it to the radius
+
+    do { // this is supposed to prevent aiorbs from being placed on top of each other but it doesn't seem to work
+        setPos(qrand() % 750, qrand() % 550);
+    } while (collidingItems().size() > 0);
 }
 
 void AIUrchin::shrink()
